66039c90ef59819d113203a1a639ad62dc1cabda
JoseG03 <104269448+JoseG03@users.noreply.github.com>
2023-06-21 15:24:57 -0500

    Update SistemaOrdenes.h

--- Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/SistemaOrdenes.h
+++ Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/SistemaOrdenes.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "OrdenBuilder.h"
 #include "Pizzas.h"
+#include "Postres.h"
 
 class SistemaOrdenes {
 
@@ -9,24 +10,30 @@ private:
 
 public:
 
-	SistemaOrdenes() { builder = new OrdenBuilder();}
-	~SistemaOrdenes() { delete builder;}
+	SistemaOrdenes() { builder = new OrdenBuilder(); }
+	~SistemaOrdenes() { delete builder; }
 
 	int menuSistemaOrdenes();
 
 	int opcionAgregar();
 	int opcionEliminar();
 	int opcionPizzas();
+	int opcionBebidas();
+	int opcionPastas();
+	int opcionPostres();
 	int opcionEliminarTipo(string tipo);
 
 	void agregarPizza();
+	void agregarPasta();
+	void agregarBebida();
+	void agregarPostre();
 	void eliminarTipo(string tipo);
-	
+
 	void agregar();
 	void eliminar();
 	void confirmar(info us);
 	void mostrarOrden();
-	
+
 };
 
 int SistemaOrdenes::menuSistemaOrdenes() {
@@ -80,11 +87,49 @@ int SistemaOrdenes::opcionPizzas() {
 	cout << "SELECCIONE LA PIZZA QUE DESEA AGREGAR" << endl << endl;
 	cout << "1. Americana" << endl;
 	cout << "2. Hawaiana" << endl;
-	cout << "3. " << endl;
-	cout << "4. " << endl;
-	cout << "5. Volver" << endl << endl; 
+	cout << "3. Mozzarela" << endl;
+	cout << "4. Pepperoni" << endl;
+	cout << "5. Vegetariana" << endl;
+	cout << "6. Suprema" << endl;
+	cout << "7. Barbacoa" << endl;
+	cout << "8. Calzone" << endl;
+	cout << "9. Volver" << endl << endl;
 	cout << "Opcion: "; cin >> op; cout << endl;
-	
+
+	return op;
+}
+
+int SistemaOrdenes::opcionBebidas() {
+	int op;
+
+	cout << "SELECCIONE LA PIZZA QUE DESEA AGREGAR" << endl << endl;
+	cout << "1. Americana" << endl;
+	cout << "2. Hawaiana" << endl;
+	cout << "3. Mozzarela" << endl;
+	cout << "4. Pepperoni" << endl;
+	cout << "5. Vegetariana" << endl;
+	cout << "6. Suprema" << endl;
+	cout << "7. Barbacoa" << endl;
+	cout << "8. Calzone" << endl;
+	cout << "9. Volver" << endl << endl;
+	cout << "Opcion: "; cin >> op; cout << endl;
+
+	return op;
+}
+
+int SistemaOrdenes::opcionPastas() {
+	int op;
+
+	cout << "SELECCIONE LA PASTA QUE DESEA AGREGAR" << endl << endl;
+	cout << "1. Spaghetti a lo Alfredo" << endl;
+	cout << "2. Pasta Puttanesca" << endl;
+	cout << "3. Tallarines en Salsa Blanca y Mushrooms" << endl;
+	cout << "4. Raviolis" << endl;
+	cout << "5. Lasagna Original Italiana" << endl;
+	cout << "6. Lasagna Especial de la Casa" << endl;
+	cout << "7. Volver" << endl << endl;
+	cout << "Opcion: "; cin >> op; cout << endl;
+
 	return op;
 }
 
@@ -106,6 +151,9 @@ void SistemaOrdenes::agregar() {
 		switch (op_agregar)
 		{
 		case 1: agregarPizza(); break;
+		case 2: agregarPasta(); break;
+		case 3: agregarBebida(); break;
+		case 4: agregarPostre(); break;
 		}
 	} while (op_agregar != 5);
 
@@ -127,7 +175,85 @@ void SistemaOrdenes::agregarPizza() {
 		builder->agregarPizza(pizza);
 		cout << "Se agrego Pizza Hawaiana al pedido! Presione Enter para continuar...";
 		cin.ignore(); cin.get(); system("cls");
-	}			
+	}
+	if (opPizzas == 3) {
+		Pizza* pizza = new Mozzarela();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Mozzarela al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 4) {
+		Pizza* pizza = new Pepperoni();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Pepperoni al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 5) {
+		Pizza* pizza = new Vegetariana();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Vegetariana al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 6) {
+		Pizza* pizza = new Suprema();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Suprema al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 7) {
+		Pizza* pizza = new Barbacoa();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Barbacoa al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 8) {
+		Pizza* pizza = new Calzone();
+		builder->agregarPizza(pizza);
+		cout << "Se agrego Pizza Calzone al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+}
+
+void SistemaOrdenes::agregarPasta() {
+
+	int opPizzas = opcionPastas();
+
+	if (opPizzas == 1) {
+		Pasta* pasta = new Spaghetti_a_lo_Alfredo();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Spaghetti a lo Alfredo al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 2) {
+		Pasta* pasta = new Pasta_Puttanesca();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Pasta Puttanesca al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 3) {
+		Pasta* pasta = new Tallarines_y_Champinones();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Tallarines en Salsa Blanca y Mushrooms al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 4) {
+		Pasta* pasta = new Raviolis();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Raviolis al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 5) {
+		Pasta* pasta = new Lasagna_Original();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Lasagna Original Italiana al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
+	if (opPizzas == 6) {
+		Pasta* pasta = new Lasagna_Especial();
+		builder->agregarPasta(pasta);
+		cout << "Se agrego Lasagna Especial de la Casa al pedido! Presione Enter para continuar...";
+		cin.ignore(); cin.get(); system("cls");
+	}
 }
 
 void SistemaOrdenes::eliminar() {
@@ -158,12 +284,12 @@ void SistemaOrdenes::eliminarTipo(string tipo) {
 			}
 		} while (op != 0);
 	}
-	system("cls");	
+	system("cls");
 }
 
-void SistemaOrdenes::confirmar(info us){
+void SistemaOrdenes::confirmar(info us) {
 	cout << "Se confirmo la orden de venta!!! Recuerde realizar el pago de su boleta dentro del sistema de pagos" << endl << endl;
-	
+
 	linea();
 	cout << "              SAPORE D'ITALIA" << endl;
 	linea();
@@ -180,7 +306,7 @@ void SistemaOrdenes::confirmar(info us){
 	cout << endl << "Presione Enter para continuar...";
 	cin.ignore(); cin.get();
 	system("cls");
-	
+
 }
 
 void SistemaOrdenes::mostrarOrden() {
