ab159dafc77ca159795ff64fac662225f04e0937
JoseG03 <104269448+JoseG03@users.noreply.github.com>
2023-06-21 12:56:58 -0500

    Update Pastas.h

--- Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pastas.h
+++ Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pastas.h
@@ -1,1 +1,32 @@
 #pragma once
+#include "ProductosPadre.h"
+
+class Spaghetti_a_lo_Alfredo : public Pizza {
+public:
+	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+};
+
+class Pasta_Puttanesca : public Pizza {
+public:
+	Pasta_Puttanesca() : Pasta("Pasta Puttanesca", 19.00) {}
+};
+
+class Tallarines_y_Champinones : public Pizza {
+public:
+	Tallarines_y_Champinones() : Pasta("Tallarines en Salsa Blanca y Mushrooms", 20.00) {}
+};
+
+class Spaghetti_a_lo_Alfredo : public Pizza {
+public:
+	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+};
+
+class Spaghetti_a_lo_Alfredo : public Pizza {
+public:
+	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+};
+
+class Spaghetti_a_lo_Alfredo : public Pizza {
+public:
+	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+};
