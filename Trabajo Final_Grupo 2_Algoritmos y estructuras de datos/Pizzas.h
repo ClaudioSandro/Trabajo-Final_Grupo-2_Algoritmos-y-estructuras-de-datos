47276301aa29518c66fa7e6cde7de3d3fd14fca5
JoseG03 <104269448+JoseG03@users.noreply.github.com>
2023-06-21 12:42:35 -0500

    Update Pizzas.h

--- Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pizzas.h
+++ Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pizzas.h
@@ -3,10 +3,40 @@
 
 class Americana : public Pizza {
 public:
-	Americana() : Pizza("Pizza Americana", 27.90) {}
+	Americana() : Pizza("Pizza Americana", 26.90) {}
 };
 
 class Hawaiana : public Pizza {
 public:
 	Hawaiana() : Pizza("Pizza Hawaiana", 32.90) {}
+};
+
+class Mozzarela : public Pizza {
+public:
+	Mozzarela() : Pizza("Pizza Mozzarela", 29.90) {}
+};
+
+class Pepperoni : public Pizza {
+public:
+	Pepperoni() : Pizza("Pizza Pepperoni", 32.90) {}
+};
+
+class Vegetariana : public Pizza {
+public:
+	Vegetariana() : Pizza("Pizza Vegetariana", 36.90) {}
+};
+
+class Suprema : public Pizza {
+public:
+	Suprema() : Pizza("Pizza Suprema", 39.90) {}
+};
+
+class Barbacoa : public Pizza {
+public:
+	Barbacoa() : Pizza("Pizza Barbacoa", 38.90) {}
+};
+
+class Calzone : public Pizza {
+public:
+	Calzone() : Pizza("Calzone", 27.90) {}
 };
