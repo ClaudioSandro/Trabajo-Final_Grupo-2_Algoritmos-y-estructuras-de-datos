0e09681a23d72dad5274de47e03cec7b3799e62c
JoseG03 <104269448+JoseG03@users.noreply.github.com>
2023-06-21 12:48:25 -0500

    Update Postres.h

--- Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Postres.h
+++ Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Postres.h
@@ -1,1 +1,22 @@
 #pragma once
+#include "ProductosPadre.h"
+
+class Pizza_de_chocolate : public Pizza {
+public:
+	Pizza_de_chocolate() : Postre("Pizza de chocolate", 19.90) {}
+};
+
+class Churro_D_ITALIA : public Pizza {
+public:
+	Churro_D_ITALIA() : Postre("Churro D'Italia (4 un)", 5.90) {}
+};
+
+class Rolls_De_Manajar : public Pizza {
+public:
+	Rolls_De_Manajar() : Postre("Rolls De Manajar (6 un)", 10.90) {}
+};
+
+class Volcan_De_Chocolate : public Pizza {
+public:
+	Volcan_De_Chocolate() : Postre("Volcan De Chocolate", 10.90) {}
+};
