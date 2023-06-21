32aec8d0b860fa3243e50fdc12d888e6d049c4b1
JoseG03 <104269448+JoseG03@users.noreply.github.com>
2023-06-21 14:30:57 -0500

    Update Pastas.h

--- Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pastas.h
+++ Trabajo Final_Grupo 2_Algoritmos y estructuras de datos/Pastas.h
@@ -16,17 +16,17 @@ public:
 	Tallarines_y_Champinones() : Pasta("Tallarines en Salsa Blanca y Mushrooms", 20.00) {}
 };
 
-class Spaghetti_a_lo_Alfredo : public Pizza {
+class Raviolis : public Pizza {
 public:
-	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+	Raviolis() : Pasta("Raviolis", 24.90) {}
 };
 
-class Spaghetti_a_lo_Alfredo : public Pizza {
+class Lasagna_Original : public Pizza {
 public:
-	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+	Lasagna_Original() : Pasta("Lasagna Original Italiana", 28.00) {}
 };
 
-class Spaghetti_a_lo_Alfredo : public Pizza {
+class Lasagna_Especial : public Pizza {
 public:
-	Spaghetti_a_lo_Alfredo() : Pasta("Spaghetti a lo Alfredo", 20.00) {}
+	Lasagna_Especial() : Pasta("Lasagna Especial de la Casa", 34.00) {}
 };
